d8107f09d374a875f4a877b6c2bdde553f7d1776
Ben Stirling <abenstirling@pm.me>
2024-01-25 16:04:30 -0800

    Initial Commit

---
+++ BotionNFC/BotionNFCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotionNFCApp.swift
+//  BotionNFC
+//
+//  Created by Ben Stirling on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BotionNFCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BotionNFC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BotionNFC
+//
+//  Created by Ben Stirling on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
